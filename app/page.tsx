7330d73afaf425fba6bdaed018c6b49954018e98
Onels <colonelsorder@gmail.com>
2025-01-31 16:58:52 +0100

    subtil correction of href # to active new vercel domain for staging branch

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <footer className="flex gap-6 flex-wrap items-center justify-center mt-10">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -46,11 +46,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          About Us
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -61,11 +61,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          CGV
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -76,7 +76,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Partners →
         </a>
       </footer>
     </div>
