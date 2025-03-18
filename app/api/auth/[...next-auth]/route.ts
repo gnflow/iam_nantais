71b437764635c123f2ef1f4d7d969bc0a8ecd9ac
Onels <colonelsorder@gmail.com>
2025-03-18 16:44:30 +0100

    update [..Next]/route.tsx to avoid linting build error

--- app/api/auth/[...next-auth]/route.ts
+++ app/api/auth/[...next-auth]/route.ts
@@ -1,5 +1,3 @@
 // api/[...next]/route.tsx
-import { auth } from "@/auth"; // Utilisation de `auth` au lieu de `handlers`
-
-export const GET = auth;
-export const POST = auth;
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers
